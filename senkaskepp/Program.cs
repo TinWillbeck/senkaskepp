5196c817806d2ea4b64ff444c7d99076b4041d85
Tin Willbeck <tin.willbeck@elev.ga.ntig.se>
2022-11-22 11:07:58 +0100

    placering av båtar

--- senkaskepp/Program.cs
+++ senkaskepp/Program.cs
@@ -3,8 +3,15 @@ using System.Numerics;
 Raylib.InitWindow(900, 900, "Sänka skepp");
 Raylib.SetTargetFPS(60);
 
+float speed = 30;
 Texture2D board = Raylib.LoadTexture("board.png");
 Texture2D enemyBoard = Raylib.LoadTexture("board.png");
+
+Rectangle boat1 = new Rectangle(100, 600, 50, 20);
+Rectangle boat2 = new Rectangle(100, 650, 70, 20);
+Rectangle boat3 = new Rectangle(100, 700, 70, 20);
+Rectangle boat4 = new Rectangle(100, 750, 100, 20);
+Rectangle boat5 = new Rectangle(100, 800, 150, 20);
 
 string currentScene = "start";
 
@@ -17,6 +24,24 @@ while (Raylib.WindowShouldClose() == false){
     if (currentScene == "game"){
 
     }
+    
+    else if (currentScene == "preGame"){
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_ONE)){
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_W)){
+                boat1.y += speed;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_A)){
+                boat1.y -= speed;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_S)){
+                boat1.x -= speed;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_D)){
+                boat1.y += speed;
+            }
+        }
+    }
+    
     else if (currentScene == "start"){
         if (Raylib.IsKeyDown(KeyboardKey.KEY_ENTER)){
             currentScene = "preGame";
@@ -37,6 +62,11 @@ while (Raylib.WindowShouldClose() == false){
     else if (currentScene == "preGame"){
         Raylib.DrawTexture(board, 400, 500, Color.WHITE);
         Raylib.DrawTexture(enemyBoard, 400, 50, Color.WHITE);
+        Raylib.DrawRectangleRec(boat1, Color.GRAY);
+        Raylib.DrawRectangleRec(boat2, Color.GRAY);
+        Raylib.DrawRectangleRec(boat3, Color.GRAY);
+        Raylib.DrawRectangleRec(boat4, Color.GRAY);
+        Raylib.DrawRectangleRec(boat5, Color.GRAY);
         
     }
     else if (currentScene == "start"){
